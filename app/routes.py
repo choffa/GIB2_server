58d5ac1a1e6d8ce30ad766ba79f0576aff06a5aa
choffa <cgjovag@gmail.com>
2018-03-15 13:52:35 +0100

    Server talks to database

--- app/routes.py
+++ app/routes.py
@@ -1,65 +1,16 @@
 from app import app, db
-from flask import jsonify, request
+from flask import jsonify, request, abort
 from data.data import Event, Point, PointProp, EventProp
 from json import dumps as jdumps
-from geojson import loads as gloads
+from geojson import loads as gloads, dumps as gdumps
 from shapely.geometry import shape
+from sqlalchemy.exc import IntegrityError
 
 hello_world_data = {'data' : 'HELLO WORLD!'}
 string_list = ["hello", "world", "plzzzz"]
 
-# Is this strictly necessary?
-@app.route('/api/events/<event_id>/points', methods=['GET'])
-def get_points(event_id):
-    return None
-
-
 @app.route('/api/events', methods=['POST'])
 def set_event():
-    return None
-
-@app.route('/api/events/nearby', methods=['GET'])
-def get_nearby_events():
-    # For now this just returns all of the mock events
-    return md.get_events()
-
-@app.route('/api/events/<event_id>', methods=['GET'])
-def get_event_by_id(event_id):
-    # For now this returns one event with the given id
-    # The event is the same for all ids
-    return md.get_event(event_id=event_id)
-
-@app.route('/api/events/<event_id>', methods=['POST'])
-def update_event(event_id):
-    return None
-
-@app.route('/api/events/<event_id>/points', methods=['POST'])
-def set_points(event_id):
-    return None
-
-
-
-# The following routes are test endpoints!!!
-@app.route('/api/test/point/<pid>')
-def send_test_point(pid):
-    return md.get_point(pid)
-
-@app.route('/api/test/event/<eid>')
-def send_test_event(eid):
-    return md.get_event(eid)
-
-@app.route('/api/test/events')
-def send_test_events():
-    return md.get_events()
-
-@app.route('/api/test/point', methods=['POST'])
-def recieve_test_point():
-    json = request.get_json()
-    # md.parse_point(json)
-    return jsonify(json)
-
-@app.route('/api/test/event', methods=['POST'])
-def recieve_test_event():
     rjson = request.get_json()
     features = rjson['features']
     plist = []
@@ -74,15 +25,107 @@ def recieve_test_event():
         point = shape(geo)
         wkt = 'SRID=4326;' + point.wkt
         plist.append(Point(point=wkt, props=proplist))
-    e = Event(points=plist)
+    
+    props = rjson['properties']
+    proplist = []
+    for key, value in props.items():
+        proplist.append(EventProp(prop_name=key, prop=value))
+
+    e = Event(points=plist, props=proplist)
     db.session.add(e)
     db.session.flush()
     db.session.commit()
     
-    # with open('test.json', 'w') as f:
-    #     json.dump(rjson, f)
-    return jsonify(rjson)
+    return gdumps(e)
 
-@app.route('/')
-def hello_world():
-    return jsonify(string_list)
+@app.route('/api/events/nearby', methods=['GET'])
+def get_nearby_events():
+    # print(request.args)
+    events = Event.query.all()
+    d = ','.join(gdumps(e) for e in events)
+    d = '['+d+']'
+    return d
+
+@app.route('/api/events/<event_id>', methods=['GET'])
+def get_event_by_id(event_id):
+    e = Event.query.filter(Event.eid == event_id).first()
+    return gdumps(e)
+
+@app.route('/api/events/<event_id>', methods=['POST'])
+def update_event(event_id):
+    return ''
+
+@app.route('/api/events/<event_id>/points', methods=['POST'])
+def set_points(event_id):
+    r = request.get_json()
+    for feature in r:
+        geom = feature['geometry']
+        coords = geom['coordinates']
+        ewkt = 'SRID=4326;Point(' + str(coords[0]) + ' ' + str(coords[1]) + ')' 
+        props = []
+        for key, value in feature['properties'].items():
+            props.append(PointProp(prop_name=key, prop=value))
+        p = Point(point=ewkt, eid=event_id, props=props)
+        try:
+            db.session.add(p)
+            db.session.flush()
+        except IntegrityError:
+            abort(400)
+    db.session.commit()
+    return ''
+
+
+
+# The following routes are test endpoints!!!
+# @app.route('/api/test/point/<pid>')
+# def send_test_point(pid):
+#     return md.get_point(pid)
+
+# @app.route('/api/test/event/<eid>')
+# def send_test_event(eid):
+#     return md.get_event(eid)
+
+# @app.route('/api/test/events')
+# def send_test_events():
+#     return md.get_events()
+
+# @app.route('/api/test/point', methods=['POST'])
+# def recieve_test_point():
+#     json = request.get_json()
+#     # md.parse_point(json)
+#     return jsonify(json)
+
+# @app.route('/api/test/event', methods=['POST'])
+# def recieve_test_event():
+#     rjson = request.get_json()
+#     features = rjson['features']
+#     plist = []
+#     for f in features:
+#         geo = f['geometry']
+#         props = f['properties']
+#         proplist = []
+#         for key, value in props.items():
+#             proplist.append(PointProp(prop_name=key, prop=value))
+#         geo = jdumps(geo)
+#         geo = gloads(geo)
+#         point = shape(geo)
+#         wkt = 'SRID=4326;' + point.wkt
+#         plist.append(Point(point=wkt, props=proplist))
+    
+#     props = rjson['properties']
+#     proplist = []
+#     for key, value in props.items():
+#         proplist.append(EventProp(prop_name=key, prop=value))
+
+#     e = Event(points=plist, props=proplist)
+#     db.session.add(e)
+#     db.session.flush()
+#     db.session.commit()
+    
+#     # with open('test.json', 'w') as f:
+#     #     json.dump(rjson, f)
+#     return jsonify(rjson)
+
+# @app.route('/')
+# def hello_world():
+#     return jsonify(string_list)
--- data/data.py
+++ data/data.py
@@ -1,12 +1,13 @@
 from app import db
 from geoalchemy2.types import Geometry
-from geojson import Feature, Point
+from geojson import Feature, Point, FeatureCollection
 from shapely.wkb import loads
 
 class Event(db.Model):
     __tablename__ = 'events'
     eid = db.Column(db.Integer, primary_key=True)
     points = db.relationship('Point')
+    props = db.relationship('EventProp')
 
     # def __init__(self, event_id, *points, **props):
     #     self.eid = int(event_id)
@@ -14,16 +15,26 @@ class Event(db.Model):
     #     self.points = [p for p in points]
 
     def __repr__(self):
-        return '{}-{}'.format(self.eid, self.points)
+        ps = []
+        for p in self.points:
+            ps.append(loads(bytes(p.point.data)).wkt)
+
+        return '{}-{}-{}'.format(self.eid, ps, self.props)
 
     @property
     def __geo_interface__(self):
         features = []
         for p in self.points:
             g = loads(bytes(p.point.data))
-            f = Feature(id=p.pid, geometry=g)
+            props = {}
+            for prop in p.props:
+                props[prop.prop_name] = prop.prop
+            f = Feature(id=p.pid, geometry=g, properties=props)
             features.append(f)
-        return {'type': 'FeatureCollection', 'id': self.eid, 'features': features}
+        props = {}
+        for prop in self.props:
+            props[prop.prop_name] = prop.prop
+        return {'type': 'FeatureCollection', 'id': self.eid, 'features': features, 'properties': props}
         
         
 class Point(db.Model):
@@ -32,10 +43,6 @@ class Point(db.Model):
     eid = db.Column(db.Integer, db.ForeignKey('events.eid'))
     props = db.relationship('PointProp')
     point = db.Column(Geometry(geometry_type='POINT', srid=4326))
-
-    @property
-    def __geo_interface__(self):
-        return {'type': 'Point', 'coordinates': self.point, 'properties': self.prop}
 
     def __repr__(self):
         return '{}-{}-{}'.format(self.pid, self.eid, self.point)
@@ -46,14 +53,7 @@ class PointProp(db.Model):
     pid = db.Column(db.Integer, db.ForeignKey('points.pid'))
     prop_name = db.Column(db.String)
     prop = db.Column(db.String)
-
-    @property
-    def __geo_interface__(self):
-        geo = {}
-        for i in range(prop):
-            geo[self.prop_name] = self.prop
-        return geo
-    
+            
     def __repr__(self):
         return '{}-{}-{}-{}'.format(self.prid, self.pid, self.prop_name, self.prop)
 
@@ -63,13 +63,6 @@ class EventProp(db.Model):
     eid = db.Column(db.Integer, db.ForeignKey('events.eid'))
     prop_name = db.Column(db.String)
     prop = db.Column(db.String)
-
-    @property
-    def __geo_interface__(self):
-        geo = {}
-        for i in range(prop):
-            geo[self.prop_name] = self.prop
-        return {geo}
-    
+            
     def __repr__(self):
         return '{}-{}-{}-{}'.format(self.prid, self.eid, self.prop_name, self.prop)
