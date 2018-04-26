0bed0b21b9040ac170ab223389b48cd9c5bbb788
choffa <cgjovag@gmail.com>
2018-04-26 15:32:03 +0200

    Added new EventStats class with table in database

--- data/data.py
+++ data/data.py
@@ -94,12 +94,12 @@ class EventProp(db.Model):
 class User(db.Model):
     __tablename__ = 'users'
     uid = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String, unique=True)
+    username = db.Column(db.String, unique=True, index=True)
     pw_hash = db.Column(db.String)
     saved_events = db.relationship('Event', secondary='my_events')
 
     def __init__(self, username, password):
-        self.username = username
+        self.username = username.lower()
         self.pw_hash = gph(password)
     
     def check_password(self, candidate):
@@ -123,6 +123,43 @@ class Time(db.Model):
         if not len(seconds):
             return 0
         return round(sum(seconds)/len(seconds))
+
+class EventStats(db.Model):
+    __tablename__ = 'event_stats'
+    sid = db.Column(db.Integer, primary_key=True)
+    uid = db.Column(db.Integer, db.ForeignKey('User.uid'), index=True)
+    eid = db.Column(db.Integer, db.ForeignKey('Event.eid'), index=True)
+    seconds_used = db.Column(db.Integer)
+    score = db.Column(db.Integer)
+
+    def __init__(self, uid, eid, hours=0, minutes=0, seconds=0, score=0):
+        self.uid = uid
+        self.eid = eid
+        self.seconds_used = hours * 3600 + minutes * 60 + seconds
+        self.score = score
+    
+    @staticmethod
+    def calc_average_time(event_id):
+        try:
+            seconds = round(db.session.query(func.avg(EventStats.seconds_used)).filter(EventStats.eid == event_id).scalar())
+        except TypeError:
+            seconds = 0
+
+        hours = seconds // 3600
+        seconds = seconds % 3600
+        minutes = seconds // 60
+        seconds = seconds % 60
+
+        return '{:02d}:{:02d}:{:02d}'.format(hours, minutes, seconds)
+    
+    @staticmethod
+    def calc_average_score(event_id):
+        try:
+            avg_score = float(db.session.query(func.avg(EventStats.score)).filter(EventStats.eid == event_id).scalar)
+        except TypeError:
+            avg_score = 0
+        return avg_score
+
 
 class DeltaTime():
     
