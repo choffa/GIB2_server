5759daee97c3be03bcd62044803d23d8f03b3822
choffa <cgjovag@gmail.com>
2018-04-26 13:11:31 +0200

    Fixed distance query

--- queries.sql
+++ queries.sql
@@ -1,6 +1,5 @@
 /* Find all points and the id of the events that they belong 
- * including start points
- */
+ * including start points */
 SELECT pid, eid, ST_AsText(point) AS point
 FROM points NATURAL JOIN event_point
 UNION ALL
@@ -9,4 +8,9 @@ FROM points JOIN events ON pid = start_point_id
 ORDER BY pid ASC;
 
 /* Query for checking points within distance */
-select pid, ST_AsText(point) as point, ST_Distance(point, ST_GeogFromText('POINT(63.4140397 10.4134941)')) as dist from points where dist < 200;
+SELECT pid, point_text, dist
+FROM (
+    select pid, ST_AsText(point) as point_text, ST_Distance(point, ST_GeogFromText('POINT(63.4140397 10.4134941)')) as dist from points
+) AS sub_table
+WHERE dist < 500
+ORDER BY dist ASC;
