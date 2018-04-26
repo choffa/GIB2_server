238521be81b4a572ffdde4d19a76533903fa07bb
choffa <cgjovag@gmail.com>
2018-04-26 12:43:17 +0200

    Added query for distance

--- queries.sql
+++ queries.sql
@@ -7,3 +7,6 @@ UNION ALL
 SELECT pid, eid, ST_AsText(point) AS point
 FROM points JOIN events ON pid = start_point_id
 ORDER BY pid ASC;
+
+/* Query for checking points within distance */
+select pid, ST_AsText(point) as point, ST_Distance(point, ST_GeogFromText('POINT(63.4140397 10.4134941)')) as dist from points where dist < 200;
